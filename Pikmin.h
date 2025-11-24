eaf5776ef373b83d2ce22032a7ab6ef9ab48b6a3
riiverk <264926w@gmail.com>
2025-11-24 16:24:47 +0900

    Pikmin class 뼈대 구성

--- Pikmin.h
+++ Pikmin.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <gl/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Pikmin {
 private:
+	GLuint VAO, VBO, EBO;
 	float x, y, z;
+	float speed, direction;
+	bool captured;
+
+	void init();
+
 public:
 	Pikmin();
 	~Pikmin();
 
-	void init();
+	void update();
+	void draw();
+	bool ifCaptured() { return captured; }
 };
